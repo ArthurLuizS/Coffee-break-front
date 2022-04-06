ce827b6afda57d94f2cecf92c4fb15a4313ccdb7
Arthur Luiz <arthurluiz36@gmail.com>
2022-04-06 19:07:03 -0300

    Feito a funcao de post na api utilizando o axios, inserindo um novo funcionario e iniciando o get

--- js/main.js
+++ js/main.js
@@ -1,5 +1,37 @@
 const url = "http://localhost:8080/funcionarios"
 const url2 = "http://localhost:8080/funcionarios/cadastrar"
+
+let form = document.getElementById("form");
+let botao = document.getElementById("btn-submit");
+
+function cadastrarFunc() {
+    let nome =  document.getElementById("name").value;
+    let cpf = document.getElementById("cpf").value;
+    let alimento = document.getElementById("alimento").value;
+    let url = "http://localhost:8080/funcionarios/cadastrar"
+    event.preventDefault();
+    funcionario = {
+        "nome": nome,
+        "cpf": cpf,
+        "alimento": alimento
+    };
+
+    cadastrar(url, funcionario);
+
+}
+
+function listar(){
+    let lista = document.getElementsByClassName("#lista")
+    event.preventDefault();
+    axios.get(url)
+    .then(response =>{
+        console.log(response)
+        const data = response.data
+   lista.textContent = JSON.stringify(data)
+    })
+    .catch(error => console.log(error))
+}
+/*
 const novoFunc = {
     nome: "Leticia",
     cpf: "11111111",
@@ -16,11 +48,12 @@ function getFunc() {
 }
 
 getFunc()
-
-function cadastrarFunc(){
-    axios.post(url2, novoFunc)
-        .then(response => 
-            alert(JSON.stringify(response.data)))
-        .catch(error => console.log(error))
+*/
+function cadastrar(url, funcionario){
+ //   event.preventDefault();
+   // console.log("funcionario = ", funcionario)
+   axios.post(url, funcionario)
+        .then(response =>  
+            console.log(JSON.stringify(response.data)))
+        .catch(error => alert( error.response.data.message)) 
 }
-cadastrarFunc()
